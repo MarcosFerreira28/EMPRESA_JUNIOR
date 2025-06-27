1f824f0cd74f3f2bebc6eeabede709705d6a5d4f
Marcos <mvmendesferreira@id.uff.br>
2025-06-27 09:03:38 -0300

    feat: adicionando condição para caso a variável a seja 0

--- questao1/questao1.js
+++ questao1/questao1.js
@@ -7,11 +7,16 @@ let delta = (b * b) - (4 * a * c);
 if(delta < 0) {
     console.log("A equação não possui raízes reais.");
 }else if(delta == 0) {
+    if(a == 0 && b == 0)
+        console.log("Erro: A e B não podem ser ambos zero.");
     let raiz = -b / (2 * a);
     console.log("A equação possui uma raiz real: " + raiz);
 }else {
+    if(a == 0)
+        console.log("Erro: A não pode ser zero, pois leva as raízes a infinito.");
     let raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
     let raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+    console.log(Math.sqrt(delta));
     console.log("A equação possui duas raízes reais: " + raiz1 + " e " + raiz2);
 }
 
