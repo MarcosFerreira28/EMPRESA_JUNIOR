dd11f0fa0088c24e298ea695b5b5f4dd51db6675
Marcos <mvmendesferreira@id.uff.br>
2025-06-27 08:55:09 -0300

    feat: adicionando funcionalidade para calcular as raízes reais

--- questao1/questao1.js
+++ questao1/questao1.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+let a = parseInt(prompt("Digite o valor do coeficiente a: "));
+let b = parseInt(prompt("Digite o valor do coeficiente a: "));
+let c = parseInt(prompt("Digite o valor do coeficiente a: "));
+
+let delta = (b * b) - (4 * a * c);
+
+if(delta < 0) {
+    console.log("A equação não possui raízes reais.");
+}else if(delta == 0) {
+    let raiz = -b / (2 * a);
+    console.log("A equação possui uma raiz real: " + raiz);
+}else {
+    let raiz1 = (-b + Math.sqrt(delta)) / (2 * a);
+    let raiz2 = (-b - Math.sqrt(delta)) / (2 * a);
+    console.log("A equação possui duas raízes reais: " + raiz1 + " e " + raiz2);
+}
+
