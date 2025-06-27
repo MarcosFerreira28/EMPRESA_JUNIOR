b00feb0ac74fd1cf50ab70f35db0c32a9da7ec9c
Marcos <mvmendesferreira@id.uff.br>
2025-06-27 09:13:44 -0300

    fix: corrigindo mensagem de prompt

--- questao1/questao1.js
+++ questao1/questao1.js
@@ -1,12 +1,12 @@
 let a = parseInt(prompt("Digite o valor do coeficiente a: "));
-let b = parseInt(prompt("Digite o valor do coeficiente a: "));
-let c = parseInt(prompt("Digite o valor do coeficiente a: "));
+let b = parseInt(prompt("Digite o valor do coeficiente b: "));
+let c = parseInt(prompt("Digite o valor do coeficiente c: "));
 
 let delta = (b * b) - (4 * a * c);
 
 if(a == 0) {
     console.log("O coeficiente 'a' não pode ser zero pois a equação se torna de primeiro grau.");
-    x = -c / b;
+    x = - (c / b);
     console.log("Nesse caso equação seria de primeiro grau e possuiria uma raiz real: " + x);
 }else if(delta < 0) {
     console.log("A equação não possui raízes reais.");
@@ -19,4 +19,3 @@ if(a == 0) {
     console.log(Math.sqrt(delta));
     console.log("A equação possui duas raízes reais: " + raiz1 + " e " + raiz2);
 }
-
