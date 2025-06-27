0d57158d8354d47833d599d10ad4d94e7c239553
Marcos <mvmendesferreira@id.uff.br>
2025-06-27 09:30:03 -0300

    fix: retirando logs desnecessários, e melhorando alerta

--- questao2/questao2.js
+++ questao2/questao2.js
@@ -2,14 +2,12 @@ let desicao = "s";
 while (desicao.toLowerCase() === "s") {
     let n = parseInt(prompt("Digite um número inteiro positivo: "));
     while (n <= 0) {
-        alert("Número inválido! Não é um inteiro positivo.");
+        alert("Número inválido! Não é um inteiro positivo, tente novamente.");
         n = parseInt(prompt("Digite um número inteiro positivo: "));
     }
     let fatorial = 1;
     for (let i = 1; i <=n; i++) {
-        console.log(fatorial);
         fatorial *= i;
-        console.log(fatorial);
     }
     console.log(`O fatorial de ${n} é: ${fatorial}`);
     desicao = prompt("Deseja calcular o fatorial de outro número? (s/n)");
