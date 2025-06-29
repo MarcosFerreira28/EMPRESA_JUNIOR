9a67ccee3ad7be2a4ea35d0cc05a132338a9ba6e
Marcos <mvmendesferreira@id.uff.br>
2025-06-29 20:10:49 -0300

    feat: adicionando checagem para caso o ano seja inválido

--- questao3/questao3.js
+++ questao3/questao3.js
@@ -1,22 +1,26 @@
-let anosDeNascimento = [2004,2002,2007,1993,1960,2010];
+let anosDeNascimento = [2004,2012,2007,1993,1960,2010,2018,2012,2003,2014,29000];
 let maiorOuMenor = [];
 let ano = 2025;
 
-for (let i =0; i<anosDeNascimento.length; i++){
+for (let i =0; i < anosDeNascimento.length; i++){
 
     let idade = ano - anosDeNascimento[i];
 
-    if (idade >= 18){
+    if (idade >= 18) {
         maiorOuMenor.push("maior");
-    }else {
+    }else if (idade < 18 && anosDeNascimento[i] <= ano) {
         maiorOuMenor.push("menor");
+    }else {
+        maiorOuMenor.push("ERRO");
     }
 }
 
 for (let i = 0; i < anosDeNascimento.length; i++) {
-    if (maiorOuMenor[i] === "maior") {
+    if (maiorOuMenor[i] == "maior") {
         console.log(`Pessoa ${i}: Maior de idade`)
-    }else {
+    }else if (maiorOuMenor[i] == "menor") {
         console.log(`Pessoa ${i}: Menor de idade`)
+    }else {
+        console.log("Esse ano é impossível ser o ano de nascimento de alguém");
     }
 }
