ea0adabac76b676b5e2b6737c5f1769a04a04867
Marcos <mvmendesferreira@id.uff.br>
2025-06-29 23:23:33 -0300

    feat: adicionando a lógica numa função

--- questao3/questao3.js
+++ questao3/questao3.js
@@ -1,26 +1,31 @@
-let anosDeNascimento = [2004,2012,2007,1993,1960,2010,2018,2012,2003,2014,29000];
-let maiorOuMenor = [];
-let ano = 2025;
+function VerificarIdades(anosDeNascimento) {
+    let maiorOuMenor = [];
+    let ano = 2025;
 
-for (let i =0; i < anosDeNascimento.length; i++){
+    for (let i =0; i < anosDeNascimento.length; i++){
 
-    let idade = ano - anosDeNascimento[i];
+        let idade = ano - anosDeNascimento[i];
 
-    if (idade >= 18) {
-        maiorOuMenor.push("maior");
-    }else if (idade < 18 && anosDeNascimento[i] <= ano) {
-        maiorOuMenor.push("menor");
-    }else {
-        maiorOuMenor.push("ERRO");
+        if (idade >= 18) {
+            maiorOuMenor.push("maior");
+        }else if (idade < 18 && anosDeNascimento[i] <= ano) {
+            maiorOuMenor.push("menor");
+        }else {
+            maiorOuMenor.push("ERRO");
+        }
+    }
+
+    for (let i = 0; i < anosDeNascimento.length; i++) {
+        if (maiorOuMenor[i] == "maior") {
+            console.log(`Pessoa ${i}: Maior de idade`)
+        }else if (maiorOuMenor[i] == "menor") {
+            console.log(`Pessoa ${i}: Menor de idade`)
+        }else {
+            console.log("Esse ano é impossível ser o ano de nascimento de alguém");
+        }
     }
 }
 
-for (let i = 0; i < anosDeNascimento.length; i++) {
-    if (maiorOuMenor[i] == "maior") {
-        console.log(`Pessoa ${i}: Maior de idade`)
-    }else if (maiorOuMenor[i] == "menor") {
-        console.log(`Pessoa ${i}: Menor de idade`)
-    }else {
-        console.log("Esse ano é impossível ser o ano de nascimento de alguém");
-    }
-}
+let anosDeNascimento = [2004,2012,2007,1993,4089,1960,2010,2018,2012,2003,2014];
+
+VerificarIdades(anosDeNascimento);
