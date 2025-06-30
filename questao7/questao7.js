b5b75f95b5868eaa41de364d4b618c3953790e87
Marcos <mvmendesferreira@id.uff.br>
2025-06-30 00:22:08 -0300

    feat: teste invertendo frase

--- questao7/questao7.js
+++ questao7/questao7.js
@@ -4,3 +4,12 @@ frase = frase.toLowerCase();
 frase = frase.split(" ");
 
 console.log(frase);
+console.log(frase[frase.length - 1]);
+
+let fraseInvertida = "";
+for (let i = frase.length - 1; i >= 0; i--) {
+    for (let j = frase[i].length - 1; j >= 0; j--){
+        fraseInvertida += frase[i][j];
+        console.log(fraseInvertida);
+    }
+}
