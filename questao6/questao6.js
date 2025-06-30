f09f6bc934016e8bf7f719a5dd2f2d42369432bd
Marcos <mvmendesferreira@id.uff.br>
2025-06-30 00:08:38 -0300

    feat: adicionando função Fibonacci

--- questao6/questao6.js
+++ questao6/questao6.js
@@ -1,1 +1,15 @@
-console.log("teste");
+function Fibonacci(n){
+
+    if (n === 1) {
+        return 0;
+    }
+    else if (n === 2) {
+        return 1;
+    }
+    else {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
+
+let n = parseInt(prompt("Digite um número inteiro positivo: "));
+console.log(Fibonacci(n))
