a5cffee0bc6b87dda784d11d118f6f0532f0527c
Marcos <mvmendesferreira@id.uff.br>
2025-06-29 23:35:04 -0300

    feat: colocando tratamento de conversão para euros

--- questao4/questao4.js
+++ questao4/questao4.js
@@ -2,3 +2,6 @@ let reais = parseFloat(prompt("Insira o valor em reais para ser convertido: "));
 
 console.log(`Valor em reais: R$ ${reais.toFixed(2)}`);
 
+let euro = reais / 6.10;
+
+console.log(`Valor em euros: € ${euro.toFixed(2)}`);
