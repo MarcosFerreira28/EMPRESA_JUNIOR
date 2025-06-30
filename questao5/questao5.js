3b5c8c913433c20cca7634f98e223432aeaf70f1
Marcos <mvmendesferreira@id.uff.br>
2025-06-29 23:49:14 -0300

    fix: adicionando para não imprimir nada no 0

--- questao5/questao5.js
+++ questao5/questao5.js
@@ -1,9 +1,9 @@
 let valor = parseInt(prompt("Digite um valor inteiro: "));
 
-if (valor%3 === 0 && valor%5 === 0) {
+if (valor%3 === 0 && valor%5 === 0 && valor !== 0) {
     console.log("fizzbuzz");
-}else if (valor%3 === 0) {
+}else if (valor%3 === 0 && valor !== 0) {
     console.log("fizz");
-}else if (valor%5 === 0) {
+}else if (valor%5 === 0 && valor !== 0) {
     console.log("buzz");
 }
