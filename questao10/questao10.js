4c3cd6836aa5d70c9e534aa16aa70513935c9a6e
Marcos <mvmendesferreira@id.uff.br>
2025-07-02 10:02:05 -0300

    feat: checando se tem algum cliente antes de tentar atender

--- questao10/questao10.js
+++ questao10/questao10.js
@@ -2,19 +2,31 @@ let opcao = 4;
 clientes = [];
 
 while (opcao != 3) {
-    opcao = parseInt(prompt("Pessoas em espera:   Digite a opção desejada:\n1 - Novo Cliente\n2 - Atender Cliente\n3 - Sair"));
+
+    opcao = parseInt(prompt("Pessoas em espera: " + clientes.join(", ") + "  Digite a opção desejada:\n1 - Novo Cliente\n2 - Atender Cliente\n3 - Sair"));
 
     switch (opcao){
         case 1:
+            
             let nome = prompt("insira o nome do cliente da ser atendido: ");
             clientes.push(nome);
+            break;
         
         case 2:
-            console.log("Cliente que está sendo atendido: " + clientes[0]);
-            clientes.shift();
+
+            if (clientes.lenght !== 0){
+                console.log("Cliente que está sendo atendido: " + clientes[0]);
+                clientes.shift();
+            }
+            else {
+                console.log("Não há clientes na fila.");
+            }
+            
+            break;
 
         default:
             console.log("Opção inválida! Insira um número de 1 a 3.");
+            break;
 
     }
 
