523351abe0cae72fe2c2019d8f6b7048e16a2656
Marcos <mvmendesferreira@id.uff.br>
2025-07-02 09:33:14 -0300

    add: adicionando lógica de receber a opção e fazer a lógica de clientes

---
+++ questao10/questao10.js
@@ -0,0 +1,22 @@
+let opcao = 4;
+clientes = [];
+
+while (opcao != 3) {
+    opcao = parseInt(prompt("Pessoas em espera:   Digite a opção desejada:\n1 - Novo Cliente\n2 - Atender Cliente\n3 - Sair"));
+
+    switch (opcao){
+        case 1:
+            let nome = promt("insira o nome do cliente da ser atendido: ");
+            clientes.push(nome);
+        
+        case 2:
+            console.log("Cliente que está sendo atendido: " + clientes[0]);
+            clientes.shift();
+            
+        default:
+            console.log("Opção inválida! Insira um número de 1 a 3.");
+
+    }
+
+
+}
