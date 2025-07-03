9f10717251fd1d318dcd1d0a32029d35e6798950
Marcos <mvmendesferreira@id.uff.br>
2025-07-02 21:28:44 -0300

    fix: retirando console.logs desnecessários

--- questao12/questao12.js
+++ questao12/questao12.js
@@ -3,19 +3,16 @@ function EncontrarElementoUnico(array){
     let igual = false; // Variável para ver se o elemento é unico ou duplicado
 
     for (let i = 0; i< array.length; i++){
-
         for (let j = 0; j < unicos.length; j++){
             if(array[i] === unicos[j]){
                 unicos.splice(j, 1);
-                console.log("O array é: " + unicos);
-
                 igual = true;
                 break;
             }
         }
+
         if (igual === false){
             unicos.push(array[i]);
-            console.log("O array é: " + unicos);
         }
         else {
             igual = false;
