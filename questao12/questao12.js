510b34ebf5a83e22262c31a7535a37f83ae50c46
Marcos <mvmendesferreira@id.uff.br>
2025-07-02 21:33:45 -0300

    add: adicionando comentários para explicar lógica de variável não muito intuitiva

--- questao12/questao12.js
+++ questao12/questao12.js
@@ -6,7 +6,7 @@ function EncontrarElementoUnico(array){
         for (let j = 0; j < unicos.length; j++){
             if(array[i] === unicos[j]){
                 unicos.splice(j, 1);
-                igual = true;
+                igual = true; // adicionado aqui para indicar que o elemento já está na lista de unicos e assim não vai inseri-lo novamente após esse for
                 break;
             }
         }
@@ -15,13 +15,12 @@ function EncontrarElementoUnico(array){
             unicos.push(array[i]);
         }
         else {
-            igual = false;
+            igual = false; // reiniciado a variável para o número seguinte
         }
     }
     return unicos[0];
 }
 
-
-array = [1,2,3,3,5,2,1,6,5];
+array = [1,2,3,3,5,10,6,2,1,6,5];
 let unico = EncontrarElementoUnico(array);
 console.log("O elemento não duplicado é: " + unico);
