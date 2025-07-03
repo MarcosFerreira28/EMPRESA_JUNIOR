adb1a438b186390157f91988a801f56e1624b481
Marcos <mvmendesferreira@id.uff.br>
2025-07-02 22:05:36 -0300

    add: adicionando função que retorna jogadores por posição

--- questao13/questao13.js
+++ questao13/questao13.js
@@ -8,7 +8,20 @@ function AdicionaJogador(nome, idade, posicao, pontuacao){
     time.push(jogador);
 }
 
+function BuscaPorPosicao(posicao){
+    let jogadoresPorPosicao = [];
+    for (let i = 0; i < time.length; i++){
+        if(time[i].posicao.toLowerCase() === posicao.toLowerCase()){
+            jogadoresPorPosicao.push(time[i]);
+        }
+    }
+    if (jogadoresPorPosicao.length === 0) {
+        console.log("Não existem jogadores para essa posição.");
+        return;
+    }
+    return jogadoresPorPosicao;
+}
 
 let time = [];
-AdicionaJogador("KAIO JORGE", 23, "atacante", 19);
+AdicionaJogador("KAIO JORGE", 23, "atacante", 19); // colocar pro usuário inserir
 console.log(time);
